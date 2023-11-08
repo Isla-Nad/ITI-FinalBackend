dc849083c6581fc146f3b4e5309cf2a3e21f0297
minasamy <monos_102000@yahoo.com>
2023-11-08 07:35:14 -0500

    medicalhistory

--- medicalHistory/api/views.py
+++ medicalHistory/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from medicalHistory.models import MedicalHistory
 from medicalHistory.api.serializer import MedicalSerlizer
@@ -8,7 +9,7 @@ from rest_framework import status
 @api_view(['GET','POST'])
 def medicalhistoryapidata(request):
     if request.method=="POST":
-          information=MedicalHistory.objects.create( patient_name=request.data['patient_name'], date_of_visit=request.data['date_of_visit'],action=request.data['action'])
+          information=MedicalHistory.objects.create( patient_name=request.data['patient_name'], date_of_visit=request.data['date_of_visit'],action=request.data['action'], Allergies=request.data['Allergies'], MedicalConditions=request.data['MedicalConditions'],PreviouDentalTreatments=request.data['PreviouDentalTreatments'],DentalConditions=request.data['DentalConditions'],DentalHygieneHabits=request.data['DentalHygieneHabits'],image=request.data['image'])
 
           information.save()
           return Response({'products':MedicalSerlizer(information).data})
@@ -21,3 +22,9 @@ def medicalhistoryapidata(request):
              pro_seralizer.append(MedicalSerlizer(info).data)
 
          return Response({"data":pro_seralizer})
+class MedicalhistoryDelete(DestroyAPIView):
+    queryset = MedicalHistory.objects.all()
+    serializer_class = MedicalSerlizer    
+class MedicalhistoryUpdate(UpdateAPIView):
+    queryset = MedicalHistory.objects.all()
+    serializer_class = MedicalSerlizer   
