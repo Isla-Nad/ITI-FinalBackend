8b65520a957c1b8252be0a645a48b71948fac1d0
Islam Nady <islamnady95@gmail.com>
2023-11-10 14:23:30 +0200

    adjustment appointments

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from appointments.views import appointment_add, appointment_book, appointment_delete, appointments_get, booked_appointments
+from appointments.views import appointment_add, appointment_book, appointment_delete, appointments_get, booked_appointments, booked_appointments_doctor, appointment_book_doctor
 
 urlpatterns = [
     path('<int:id>', appointments_get, name='appointments.appointments_get'),
@@ -10,4 +10,8 @@ urlpatterns = [
          name='appointments.appointment_book'),
     path('booked/<int:id>', booked_appointments,
          name='appointments.booked_appointments'),
+    path('book/doctor/<int:id>', appointment_book_doctor,
+         name='appointments.appointment_book_doctor'),
+    path('booked/doctor/<int:id>', booked_appointments_doctor,
+         name='appointments.booked_appointments_doctor'),
 ]
