215506f6693e68c74977f77bb1f4ce38b02e04af
minasamy <monos_102000@yahoo.com>
2023-11-10 07:20:05 -0500

    adminPanel medicalhistory

--- medical_history/models.py
+++ medical_history/models.py
@@ -7,18 +7,18 @@ class MedicalHistory(models.Model):
         User, related_name='owner', on_delete=models.CASCADE)
     patient_name = models.CharField(max_length=100)
     date_of_visit = models.DateField()
-    allergies = models.TextField(
-        default="Enter Allergies", null=True, blank=True)
-    medical_conditions = models.TextField(
-        default="Enter Medical Conditions", null=True, blank=True)
-    previous_dental_treatments = models.TextField(
-        default="Enter Previous Dental Treatments", null=True, blank=True)
-    dental_conditions = models.TextField(
-        default="Enter Dental Conditions", null=True, blank=True)
-    dental_hygiene_habits = models.TextField(
-        default="Enter Dental Hygiene Habits", null=True, blank=True)
-    specific_dental_concerns = models.TextField(
-        default="Enter Specific Dental Concerns", null=True, blank=True)
+    allergies = models.CharField(
+        default="", null=True, blank=True)
+    medical_conditions = models.CharField(
+        default="", null=True, blank=True)
+    previous_dental_treatments = models.CharField(
+        default="", null=True, blank=True)
+    dental_conditions = models.CharField(
+        default="", null=True, blank=True)
+    dental_hygiene_habits = models.CharField(
+        default="", null=True, blank=True)
+    specific_dental_concerns = models.CharField(
+        default="", null=True, blank=True)
     image = models.ImageField(
         upload_to='medical_history/images/', max_length=200, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -26,3 +26,5 @@ class MedicalHistory(models.Model):
 
     def __str__(self):
         return f"{self.patient_name}"
+    def get_image_url(self):
+        return f'/media/{self.image}'
