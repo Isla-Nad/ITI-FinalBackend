682ec40981ac3033fadea6a1628d5faf9a6b4f85
Islam Nady <islamnady95@gmail.com>
2023-11-13 12:54:23 +0200

    clinic search

--- clinics/urls.py
+++ clinics/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 
-from clinics.views import index,show,delete,create_case,create_clinic_image
-
+from clinics.views import index, search_clinics, show, delete, create_case, create_clinic_image
 
 
 urlpatterns = [
-    path("",index,name='clinics.index'),
-    path("show/<int:id>",show,name='clinics.show'),
-    path("delete/<int:id>",delete,name='clinics.delete'),
-    path("createcase",create_case,name='create_case'),
-    path("createclinicimage",create_clinic_image,name='create_clinic_image'),
-    
+    path("", index, name='clinics.index'),
+    path("show/<int:id>", show, name='clinics.show'),
+    path("delete/<int:id>", delete, name='clinics.delete'),
+    path("createcase", create_case, name='create_case'),
+    path("createclinicimage", create_clinic_image, name='create_clinic_image'),
+    path('search/clinics', search_clinics, name='clinics.search_clinics'),
 ]
--- clinics/views.py
+++ clinics/views.py
@@ -1,34 +1,36 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from clinics.models import Clinic,Cases,ClinicImages
-from clinics.serializers import ClinicSerializer,CaseSerializer,ClinicImageSerializer
+from clinics.models import Clinic, Cases, ClinicImages
+from clinics.serializers import ClinicSerializer, CaseSerializer, ClinicImageSerializer
 from rest_framework import status
 
-@api_view(['GET','POST'])
-def index (request):
+
+@api_view(['GET', 'POST'])
+def index(request):
     if request.method == 'POST':
-        clinic = Clinic.objects.create(name=request.data['name'], 
-                                        desc=request.data['desc'],
-                                        image=request.data['image'],
-                                        address=request.data['address']
-                                        )
+        clinic = Clinic.objects.create(name=request.data['name'],
+                                       desc=request.data['desc'],
+                                       image=request.data['image'],
+                                       address=request.data['address']
+                                       )
         clinic.save()
-        return Response({'clinic':ClinicSerializer(clinic).data})
+        return Response({'clinic': ClinicSerializer(clinic).data})
     elif request.method == 'GET':
         clinics = Clinic.get_all_clinics()
         serialized_clinics = []
         for cln in clinics:
             serialized_clinics.append(ClinicSerializer(cln).data)
-            
-        return Response({"clinics":serialized_clinics})
-    
-@api_view(['GET'])   
-def show(request,id):
+
+        return Response({"clinics": serialized_clinics})
+
+
+@api_view(['GET'])
+def show(request, id):
     clinic = Clinic.objects.get(id=id)
     cases_for_clinic = Cases.objects.filter(clinic_id=id)
     images_for_clinic = ClinicImages.objects.filter(clinic_id=id)
-    
+
     # Serialize the clinic and cases queryset as dictionaries
     clinic_data = ClinicSerializer(clinic).data
     cases_data = CaseSerializer(cases_for_clinic, many=True).data
@@ -36,16 +38,14 @@ def show(request,id):
 
     return Response({'clinic': clinic_data, 'cases': cases_data, 'images': images_data})
 
-@api_view(['POST'])   
-def delete(request,id):
+
+@api_view(['POST'])
+def delete(request, id):
     clinic = Clinic.objects.get(id=id)
     clinic.delete()
     return Response({"message": "Clinic deleted successfully"})
 
 
-
-    
-    
 @api_view(['POST'])
 def create_case(request):
     serializer = CaseSerializer(data=request.data)
@@ -62,3 +62,16 @@ def create_clinic_image(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def search_clinics(request):
+    search_query = request.GET.get('search_query', '')
+    clinics = Clinic.objects.all()
+
+    if search_query:
+        clinics = clinics.filter(name__icontains=search_query)
+
+    serializer = ClinicSerializer(clinics, many=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
